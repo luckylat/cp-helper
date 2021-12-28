4d632616607271b3fb9e1d05819af2ef6e79d3f3
luckYrat <lacklat.key@gmail.com>
2021-12-28 11:21:58 +0900

    ファイル構成の変更

---
+++ api/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type User struct {
+	ID     string `json:"id"`
+	Today  bool   `json:"today"`
+	Streak int    `json:"streak"`
+}
+
+var users = []User{{
+	ID:     "CleyL",
+	Today:  true,
+	Streak: 50,
+}, {
+	ID:     "tourist",
+	Today:  false,
+	Streak: 0,
+}}
+
+func main() {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		var buf bytes.Buffer
+		enc := json.NewEncoder(&buf)
+		if err := enc.Encode(&users); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(buf.String())
+
+		_, err := fmt.Fprint(w, buf.String())
+		if err != nil {
+			return
+		}
+	}
+
+	http.HandleFunc("/api/user", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
